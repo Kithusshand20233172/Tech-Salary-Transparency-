19314e9cc17c4df35d0bc24ac6d9b6505a66814a
KithusshandR <kithusshandraveendran@gmail.com>
2026-02-15 09:46:31 +0530

    Cleared repository

--- IdentityService.Api/DTOs/AuthDtos.cs
+++
@@ -1,8 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace IdentityService.Api.DTOs;
-
-public record RegisterRequest([Required][EmailAddress] string Email, [Required] string Password);
-public record LoginRequest([Required][EmailAddress] string Email, [Required] string Password);
-public record AuthResponse(string Token, string RefreshToken, string Email);
-public record RefreshTokenRequest(string RefreshToken);
--- SalaryService.Api/DTOs/AuthDtos.cs
+++
@@ -1,7 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace IdentityService.Api.DTOs;
-
-public record RegisterRequest([Required][EmailAddress] string Email, [Required] string Password);
-public record LoginRequest([Required][EmailAddress] string Email, [Required] string Password);
-public record AuthResponse(string Token, string Email);
